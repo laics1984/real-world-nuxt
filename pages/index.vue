9c2fbd10d8d8c6c4cb3c6a02d20002049c4aa350
Benjamin - workstation <benjamin@webtree.my>
2020-04-18 15:57:06 +0800

    async

--- pages/index.vue
+++ pages/index.vue
@@ -13,28 +13,26 @@
 <script>
 import EventCard from '@/components/EventCard.vue'
 export default {
-  components: {
-    EventCard
-  },
-  asyncData({ $axios, error }) {
-    return $axios
-      .get('http://localhost:3000/events')
-      .then((response) => {
-        return {
-          events: response.data
-        }
-      })
-      .catch((e) => {
-        error({
-          statusCode: 503,
-          message: 'Unable to fetch events at this time. Please try again.'
-        })
-      })
-  },
   head() {
     return {
       title: 'Event Listing'
     }
+  },
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.get('http://localhost:3000/events')
+      return {
+        events: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time. Please try again.'
+      })
+    }
+  },
+  components: {
+    EventCard
   }
 }
 </script>
